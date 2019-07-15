bae329df2d5a1570332526c27994a87eb42744bb
benettlinger <39038496+benettlinger@users.noreply.github.com>
2019-07-15 15:48:51 -0500

    Update Motor_Controller_Test.ino

--- Test Files/Motor_Controller_Test/Motor_Controller_Test.ino
+++ Test Files/Motor_Controller_Test/Motor_Controller_Test.ino
@@ -1,29 +1,36 @@
-//This code currently has no safety shutoff 
+
 #define pwm1 23
 #define pwm2 22
 #define led1 5
 #define led2 6
 #define ledOnBoard 13
 
-int spdInt = 0;
+#define ls1 9 //limit switch 1
+#define ls2 10 //limit switch 2
+
+#define cha1 16
+#define chb1 17
+#define cha2 14
+#define chb2 15
 
 void setup() {
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(ledOnBoard, OUTPUT);
-  
-  pinMode(ls1, INPUT);
-  pinMode(ls2, INPUT);
+  delay(2000);
+  Serial.println("Starting Motor Controller Test");
+  pinMode(ls1, INPUT_PULLUP);
+  pinMode(ls2, INPUT_PULLUP);
 
   pinMode(pwm1, OUTPUT);
   pinMode(pwm2, OUTPUT);
-
   //Initial Outputs
-  analogWrite(pwm2, 255);
+  digitalWrite(pwm1, 0);
+  digitalWrite(pwm2, 0);
 }
-
+int dInt;
 void loop() {
-  delay(500);
-  analogWrite(pwm1, 64);
-  spdInt = spdInt + 5;
+//  Serial.println("Starting Run Loop");
+//  while ((digitalRead(ls1)!=1) && (digitalRead(ls2)!=1)){
+    digitalWrite(pwm1, 100); //Value from 0-255
+    digitalWrite(pwm2, 0); //0-~127 is counterclockwise >128 is clockwise
+//  }
+//  digitalWrite(pwm1, 0);
 }
